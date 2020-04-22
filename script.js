3326747f0c2e9185433549b635007b1bf456b39d
ArmaniLeSure <54874404+ArmaniLeSure@users.noreply.github.com>
2020-04-21 22:03:25 -0500

    completely dropped the ball and gave each character like different ids and names.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function init(){
-	var textIn = document.getElementById('enterimput');
-	var textOut = document.getElementById('pressButt');
+	var textIn = document.getElementById('entryinput');
+	var textOut = document.getElementById('entrybutton');
 	
 	textOut.addEventListener('click', function() {
 		alert('Armani LeSure: ' + textIn.value);
-		document.getElementById('entryoutput').innerHTML = textIn.value;
+		document.getElementById('textoutput').innerHTML = textIn.value;
 		});
 }
 window.addEventListener('load', init); 
