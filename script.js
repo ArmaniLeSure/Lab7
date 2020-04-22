e11672c3141bd340eb588cdfe6539813032049ab
ArmaniLeSure <54874404+ArmaniLeSure@users.noreply.github.com>
2020-04-21 21:58:50 -0500

    finishing modifying script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function init(){
+	var textIn = document.getElementById('enterimput');
+	var textOut = document.getElementById('pressButt');
 	
-	
-	
-	
-	
+	textOut.addEventListener('click', function() {
+		alert('Armani LeSure: ' + textIn.value);
+		document.getElementById('entryoutput').innerHTML = textIn.value;
+		});
 }
 window.addEventListener('load', init); 
